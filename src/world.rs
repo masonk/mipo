bc283e2b1c42e83254e970c8383ef28f3874219f
Mason Kramer <mason@masonkramer.net>
2024-07-15 09:17:13 -0400

    Make the world bigger and change the lighting to a single directional light

--- src/world.rs
+++ src/world.rs
@@ -88,7 +88,7 @@ fn make_spawn_floor(
                 PbrBundle {
                     mesh: shaded_handle,
                     material: white_material.clone(),
-                    transform: Transform::from_scale(Vec3::new(1., 150.0, 1.0)),
+                    transform: Transform::from_scale(Vec3::new(3., 300., 3.0)),
                     ..default()
                 },
                 RigidBody::Fixed,
@@ -106,36 +106,21 @@ fn make_spawn_floor(
             });
     }
 }
+
 fn spawn_light(mut commands: Commands) {
-    let grid_size = 250.;
-    let light_grid_size = 3;
-    let interval = grid_size / light_grid_size as f32;
-
-    info!(
-        "Spawning {}x{} point lights",
-        light_grid_size, light_grid_size
-    );
-
-    for x in 0..light_grid_size {
-        for z in 0..light_grid_size {
-            let transform = Transform::from_xyz(x as f32 * interval, 150., z as f32 * interval);
-            commands.spawn((
-                PointLightBundle {
-                    point_light: PointLight {
-                        color: Color::srgba(1.0, 1.0, 1.0, 1.000),
-                        intensity: 2e8,
-                        range: 500.,
-                        radius: 25.,
-                        shadows_enabled: true,
-                        ..default()
-                    },
-                    transform,
-                    ..default()
-                },
-                Name::new(format!("world_light_{x}_{z}")),
-            ));
-        }
-    }
+    commands.spawn((
+        DirectionalLightBundle {
+            directional_light: DirectionalLight {
+                illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(-50., 900., -50.)
+                .looking_at(Vec3::new(150., 150., 150.), Vec3::Y),
+            ..default()
+        },
+        Name::new(format!("world_directional_light")),
+    ));
 }
 
 fn leash_system(
